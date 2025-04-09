2e353c513a1b775a9173a3e25346681b72bf09ae
Celeste Ang <celesteanglm@gmail.com>
2025-04-09 22:21:15 +0800

    fix: remove relative paths

--- plugins/acp/acp_plugin_gamesdk/acp_plugin.py
+++ plugins/acp/acp_plugin_gamesdk/acp_plugin.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 from game_sdk.game.agent import WorkerConfig
 from game_sdk.game.custom_types import Function, FunctionResultStatus
-from plugins.twitter.twitter_plugin_gamesdk.twitter_plugin import TwitterPlugin
-from plugins.twitter.twitter_plugin_gamesdk.game_twitter_plugin import GameTwitterPlugin
+from twitter_plugin_gamesdk.twitter_plugin import TwitterPlugin
+from twitter_plugin_gamesdk.game_twitter_plugin import GameTwitterPlugin
 from acp_plugin_gamesdk.acp_client import AcpClient
 from acp_plugin_gamesdk.acp_token import AcpToken
 from acp_plugin_gamesdk.interface import AcpJobPhasesDesc, IInventory
