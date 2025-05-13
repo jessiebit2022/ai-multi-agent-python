6c74f0384aeb9abd0a300e84ec2042119d309167
Celeste Ang <celesteanglm@gmail.com>
2025-05-13 11:09:30 +0800

    [acp plugin] fix: revert to old version of GAME twitter plugin

--- plugins/acp/examples/reactive/test_buyer.py
+++ plugins/acp/examples/reactive/test_buyer.py
@@ -11,6 +11,7 @@ from dacite.config import Config
 from rich import print, box
 from rich.panel import Panel
 from twitter_plugin_gamesdk.twitter_plugin import TwitterPlugin
+from twitter_plugin_gamesdk.game_twitter_plugin import GameTwitterPlugin
 
 def ask_question(query: str) -> str:
     return input(query)
@@ -70,7 +71,7 @@ def main():
                 "https://base-sepolia-rpc.publicnode.com/",  # RPC
                 "https://acpx-staging.virtuals.io/api"
             ),
-            twitter_plugin=TwitterPlugin(options),
+            twitter_plugin=GameTwitterPlugin(options),
             on_evaluate=on_evaluate,
             on_phase_change=on_phase_change
         )
@@ -82,8 +83,8 @@ def main():
     
     def post_tweet(content: str, reasoning: str) -> Tuple[FunctionResultStatus, str, dict]:
         if (acp_plugin.twitter_plugin is not None):
-            post_tweet_fn = acp_plugin.twitter_plugin.twitter_client.create_tweet
-            post_tweet_fn(text=content)
+            post_tweet_fn = acp_plugin.twitter_plugin.get_function('post_tweet')
+            post_tweet_fn(content, None)
             return FunctionResultStatus.DONE, "Tweet has been posted", {}
         
         return FunctionResultStatus.FAILED, "Twitter plugin is not initialized", {}
